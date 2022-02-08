9be624d36396d61bec33e649eebc85c5c483a8a9
Anthony Martinez <straff@Anthonys-MBP.fios-router.home>
2022-02-08 14:07:44 -0500

    added and changed main

---
+++ main.js
@@ -0,0 +1,36 @@
+const prompt = require("prompt-sync")();
+
+let choice = (prompt("Would you like to combine or deconstruct (c / d): "));
+
+if (choice === "c"){
+let color1 = (prompt("Select your first color (red, blue,yellow): "));
+let color2 = (prompt("Select your second color (red, blue, yellow):  "));
+// construct code
+if (color1.includes("red" || "blue") && color2.includes("blue" || "red")){
+    console.log("purple")
+}
+else if (color1.includes("red" || "yellow") && color2.includes("yellow" || "red")){
+    console.log("orange")
+}
+else if (color1.includes("blue" || "yellow") && color2.includes("yellow" || "blue")){
+    console.log("green")
+}}
+// deconstruct
+if(choice === "d"){
+let color = (prompt(" Select the color to be deconstructed: "))
+
+if (color.includes("purple" || "Purple")){
+    console.log("red + blue")
+}
+else if (color.includes("orange" || "Orange")){
+    console.log("red + yellow")
+}
+else if (color.includes("green" || "Green")){
+    console.log("yellow + blue")
+}}
+
+
+
+
+
+   
